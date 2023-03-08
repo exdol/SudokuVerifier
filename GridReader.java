35453fccc53e7b38cc8dc91018d1e2aa503c5d3e
douglasglover3 <douglasglover3@gmail.com>
2023-03-08 18:46:13 -0500

    Updated solver to use file reader

--- GridReader.java
+++ GridReader.java
@@ -1,24 +1,20 @@
-package multithreaded;
+
 
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class Runner {
-    public static int solutionCount = 0;
-    
-    public static void main(String[] args) { 
+public class GridReader {
+    public int[][] getGridFromFile(String[] args) { 
         File inputFile = null;
         Scanner in = null;
-
-        System.out.println("Stargint multithreaded approach");
 
         if (args.length == 0) {
             System.out.println("No file specified");
             System.exit(1);
         }
 
-        inputFile = new File(args[0]);
+        inputFile = new File("./boards/" + args[0]);
         try {
             in = new Scanner (inputFile);
         } catch (FileNotFoundException e) {
@@ -35,19 +31,6 @@ public class Runner {
                 grid[i][j] = currentElement;
             }
         }
-
-        long start = System.currentTimeMillis();
-        // Algorithm
-        long end = System.currentTimeMillis();
-
-        if (solutionCount == 0) {
-            System.out.print("No solution.");
-        } else if (solutionCount > 1) {
-            System.out.print(solutionCount + " solutions found");
-        } else {
-            System.out.print("Solution found");
-        }
-
-        System.out.println("Processing time: " + (end - start) + " ms");
+        return grid;
     }
 }
